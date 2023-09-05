19d65a4ed633f608ce05be90d946639303ceaddc
Alex Choi <alexander.e.choi@gmail.com>
2023-09-05 08:29:08 -0700

    add a function for the general modeled probability that takes any number of crossings at any length in any dim at any spacing. Only handles N=1

--- src/modeling.py
+++ src/modeling.py
@@ -5,7 +5,7 @@ from scipy import special as spec
 
 
 def prob_of_crossing_vs_length(
-    lengths: np.array, dim: int, C: int = 0, N: int = 1, S=None
+    lengths: np.array, dim: int, C: int = 0, N: int = 1, S=np.array([1])
 ):
     """Calculate the probability of crossing more than some number of
     hyperplanes with a hyperdimensional needle. Lots to do to clean this function up
@@ -21,37 +21,53 @@ def prob_of_crossing_vs_length(
     Returns:
         A numpy array the same length as `lengths` with the probability of
         meeting the crossing criteria."""
-    if dim < 3:
-        return None
-    if C == 0 and N == 1:
-        r_s = lengths[lengths <= 1]
-        r_l = lengths[lengths > 1]
-        p_s = r_s * spec.factorial2(dim - 2) / spec.factorial2(dim - 1)
-        if dim % 2 == 0:
-            scale = 2 / np.pi
-            loop_base = np.arccos(1 / r_l)
-        else:
-            scale = 1
-            loop_base = 1 - 1 / r_l
+    if dim < 2:
+        raise ValueError("dim must be greater than 1")
+    if N == 1:
+        i_s = lengths < S[0] * (C - 1)
+        i_m = (lengths >= S[0] * (C - 1)) * (lengths < S[0] * C)
+        i_l = lengths >= S[0] * C
 
-        loop_sum = 0
-        for j in range(1, 1 + int((dim - 2) / 2)):
-            loop_sum += (
-                spec.factorial2(dim - 2 - 2 * j)
-                / spec.factorial2(dim - 1 - 2 * j)
-                * (r_l**2 - 1) ** ((dim - 1 - 2 * j) / 2)
-                / r_l ** (dim - 2 * j)
-            )
-        p_l = (
-            spec.factorial2(dim - 2)
-            / spec.factorial2(dim - 1)
-            * r_l
-            * (1 - ((r_l**2 - 1) ** 0.5 / r_l) ** (dim - 1))
-            + loop_base
-            - loop_sum
+        r_s = lengths[i_s]
+        r_m = lengths[i_m]
+        r_l = lengths[i_l]
+
+        prob_s = np.zeros_like(r_s)
+        prob_m = prob_of_small_r(r_m, dim, C, N, S)
+        prob_l = prob_of_small_r(r_l, dim, C, N, S) - prob_of_small_r(
+            r_l, dim, C + 1, N, S
         )
-        probability = scale * np.concatenate((p_s, p_l))
 
+        probability = np.concatenate((prob_s, prob_m, prob_l))
+    else:
+        probability = np.zeros_like(lengths)
+    return probability
+
+
+def prob_of_small_r(lengths, dim, C, N, S):
+    if N == 1:
+        r = lengths[lengths > S[0] * (C - 1)]
+        gamma = S[0] * (C - 1) / r
+        loop_sum = np.sum(
+            [
+                spec.beta((dim - 2 * i) / 2, 0.5) / (1 - gamma**2) ** i
+                for i in range(1, int((dim - 2) / 2) + 1)
+            ],
+            axis=0,
+        )
+        if dim % 2 == 0:
+            parity_shape = 2 * gamma * np.arccos(gamma)
+        else:
+            parity_shape = np.pi * gamma * (1 - gamma)
+        probability = (
+            r
+            / (np.pi * S[0])
+            * (
+                (1 - gamma**2) ** ((dim - 1) / 2)
+                * (spec.beta(dim / 2, 0.5) + gamma**2 * loop_sum)
+                - parity_shape
+            )
+        )
     return probability
 
 
@@ -73,32 +89,3 @@ def prob_of_crossing_vs_dim(
         A numpy array the same length as `lengths` with the probability of
         meeting the crossing criteria."""
     raise (NotImplementedError)
-    if C == 0 and N == 1:
-        r = length
-        p_s = length * spec.factorial2(dims - 2) / spec.factorial2(dims - 1)
-        if dim % 2 == 0:
-            scale = 2 / np.pi
-            loop_base = np.arccos(1 / r_l)
-        else:
-            scale = 1
-            loop_base = 1 - 1 / r_l
-
-        loop_sum = 0
-        for j in range(1, 1 + int((dim - 2) / 2)):
-            loop_sum += (
-                spec.factorial2(dim - 2 - 2 * j)
-                / spec.factorial2(dim - 1 - 2 * j)
-                * (r_l**2 - 1) ** ((dim - 1 - 2 * j) / 2)
-                / r_l ** (dim - 2 * j)
-            )
-        p_l = (
-            spec.factorial2(dim - 2)
-            / spec.factorial2(dim - 1)
-            * r_l
-            * (1 - ((r_l**2 - 1) ** 0.5 / r_l) ** (dim - 1))
-            + loop_base
-            - loop_sum
-        )
-        probability = scale * np.concatenate((p_s, p_l))
-
-    return probability
